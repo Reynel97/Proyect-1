f2a05ad5b33b1f63b003ebd8bac915ba8caee715
Reinaldo Jose <reynaldojosedelacruzrojano@gmail.com>
2023-06-28 16:39:59 -0500

    proyect-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
 
+import {useState} from 'react'
 import './App.css'
+import UserCard from './components/userCard'
+import phrases from './data/phrases.json'
+
+
 
 function App() {
-  
+
+
+const photo = ['/fortuna-2/fortuna-2/fondo1.png', '/fortuna-2/fortuna-2/fondo2.png', '/fortuna-2/fortuna-2/fondo3.png', '/fortuna-2/fortuna-2/fondo4.png']
+const [index, useIndex ] = useState(0)
+const [foto, useFoto] = useState(0)
+
+const cambiarFrase = () => {
+    if(foto < photo.length -1) {
+       useFoto(foto +1)
+    }else{
+      useFoto(0)
+    }
+      
+    useIndex(Math.floor(Math.random()*phrases.length))
+}
+
+document.body.style = `background-image: url(${photo[foto]})`
+
 
   return (
-    <>
-  
-    
-    </>
+    <section className='set'>
+    <h1 className="titulo">GALLETAS DE LA FORTUNA</h1>
+  <UserCard data = {phrases[index]}/>
+ 
+   <button className='Ver' onClick = {cambiarFrase}>
+       Ver Suerte
+    </button>
+
+    </section>
   )
 }
 
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,17 @@
+const UserCard = (props) => {
+
+    const {data} = props
+ 
+     return(
+        <article className="contenedor">
+         <div className="Caja-suerte">
+             <p className="frase1">{data.phrase}</p>
+         </div>
+         <div className="fuente">
+             <p className="frase">{data.author}</p>
+         </div>
+        </article>
+     )
+ }
+ 
+ export default UserCard
